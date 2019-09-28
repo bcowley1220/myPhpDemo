cceb9259d0587b8c771a89133dec1b199752a158
Brendan Cowley <b.cowley1220@gmail.com>
2019-09-28 15:01:12 -0400

    basic functions & form

--- index.php
+++ index.php
@@ -1,2 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP Demo</title>
+</head>
+<body>
+  
+<form method="GET">
+<label for="person"></label>
+<input type="text" name="person" id="">
+<button>Submit</button>
+</form>
+<?php 
+  echo 'Hi There ';
+  $name = $_GET['person'];
+  print $name.' is a cool guy';
+  //echo 10 / 2;
+?>
+
 <?php
-  echo "Hi there!";
+  echo strlen('Hi Daniel');
+  echo  str_word_count('The string length of sayHello is');
+  echo strrev('Lets reverse this');
+  echo strpos('Hi Brendan', 'Hi'); 
+  echo str_replace('Brendan', 'Seamus', 'Hi Brendan');
+  // echo strlen($sayHello.value)
+?>
+
+
+</body>
+</html>
